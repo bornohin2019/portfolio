b94fab7f23a4e91cccaba98562627a413dd9a5a2
bornohin2019 <bornohin2019@gmail.com>
2025-02-25 13:09:56 +0600

    picture fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,56 +5,62 @@ import Typewriter from "typewriter-effect";
 import { Reveal, Slide, Fade } from "react-awesome-reveal";
 
 const Home = () => {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(endpoints.home, { method: "GET" });
+        const res = await response.json();
+        setData(res);
+      } catch (err) {
+        console.error("Home fetch Error", err);
+      }
+    };
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(endpoints.home, { method: "GET" });
-                const res = await response.json();
-                setData(res);
-            } catch (err) {
-                console.error("Home fetch Error", err);
-            }
-        };
-        fetchData();
-    }, []);
-
-
-
-    return data ? (
-        <div id="/" className="home">
-            <Reveal duration={3000} triggerOnce>
-            <div className="homeimage">
-            {/* <img src={data.profilePic.source} alt="ProfilePic" /> */}
-            <img 
-    src={process.env.PUBLIC_URL + "/images/rabbani_t.png"} 
-    alt="ProfilePic" 
-    style={{ width: "350px", height: "350px", objectFit: "cover" }} 
-/>
+  return data ? (
+    <div id="/" className="home">
+      <Reveal duration={3000} triggerOnce>
+        <div className="homeimage">
+          <img
+            src={process.env.PUBLIC_URL + "/images/rabbani_t.png"}
+            alt="ProfilePic"
+            style={{
+              width: "350px",
+              height: "350px",
+              objectFit: "cover",
+              borderRadius: "50%",
+              border: "5px solid #fff",
+              boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+            }}
+          />
         </div>
-
-            </Reveal>
-            <Fade direction="right" duration={3000} cascade damping={1e3} triggerOnce>
-                <div className="hometext">
-                    <h1 className="name">{data.name}</h1>
-                    <div className="textanimation">
-                        <h2 className="im">I'm</h2>
-                        <span>&nbsp;</span>
-                        <Typewriter
-                            options={{
-                                strings: data.roles,
-                                autoStart: true,
-                                loop: true
-                            }}
-                        />
-                    </div>
-                    <div className="home-paragraph"><p>{data.paragraph}</p></div>
-                </div>
-            </Fade>
+      </Reveal>
+      <Fade direction="right" duration={3000} cascade damping={1e3} triggerOnce>
+        <div className="hometext">
+          <h1 className="name">{data.name}</h1>
+          <div className="textanimation">
+            <h2 className="im">I'm</h2>
+            <span>&nbsp;</span>
+            <Typewriter
+              options={{
+                strings: data.roles,
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
+          <div className="home-paragraph">
+            <p>{data.paragraph}</p>
+          </div>
         </div>
-    ) : (<FallbackSpinner />)
-}
+      </Fade>
+    </div>
+  ) : (
+    <FallbackSpinner />
+  );
+};
 
-export default Home
+export default Home;
